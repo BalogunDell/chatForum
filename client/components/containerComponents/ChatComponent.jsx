5a34cf40c35d09a94fc3d0683c1e705a2b2aa694
Balogun Abiodun Delight <delighteddell@gmail.com>
2018-02-16 11:59:08 +0100

    feature(chat-screen): add chat screen design to application

--- client/components/containerComponents/ChatComponent.jsx
+++ client/components/containerComponents/ChatComponent.jsx
@@ -6,7 +6,7 @@ import socket from '../../assets/js/chat';
 /** 
  * @description ChatComponent
  * 
- * @returns {jJSX} DOM representation
+ * @returns {JSX} DOM representation
 */
 class ChatComponent extends React.Component {
   constructor(props){
@@ -15,13 +15,58 @@ class ChatComponent extends React.Component {
   }
 render() {
   return(
-      <div className= 'home-container'>
-          <div className="centered-intro">
-            <h1>
-              Welcome to the chat screen
-            </h1>
+    <div className= 'home-container'>
+      <div className="chat-container">
+
+        {/* Chat screen first row */}
+        <div className="row">
+          {/* First column holds chat screen navigation */}
+          <div className="col-sm-12 col-lg-4 chat-screen-navigation">
+
+            <div className="general-header">
+              <h4>
+                Users online
+              </h4>
+            </div>
+
+            <div className="users-online">
+              <ul>
+                <li>Seun koko</li>
+                <li>Frank</li>
+                <li>Mike32</li>
+                <li>Brad Pit</li>
+              </ul>
+             </div>
+
           </div>
+
+          {/* First column holds chat board */}
+          <div className="col-sm-12 col-lg-8 chat-board">
+            <div>
+              <h4>Chat History</h4>
+            </div>
+
+            <div className="messages">
+              <p>Laolu: dfajdfnjdfndanfadfdfajdfnjdfndanfadfdfajdfnjdfndanfadfdfajdfnjdfndanfadfdfajdfnjdfndanfadfdfajdfnjdfndanfadfdfajdfnjdfndanfadfdfajdfnjdfndanfadfdfajdfnjdfndanfadfdfajdfnjdfndanfadf</p>
+              <p>Femi: dfajdfnjdfndanfadf</p>
+            </div>
+
+          {/* Input message holder */}
+            <div className="row messaging-center">
+
+              <div className="col-sm-12">
+                <textarea
+                  name="newMessage" 
+                  id="newMessage" >
+                </textarea>
+              </div>
+
+            </div>
+
+          </div>
+        </div>
       </div>
+    </div>
     );
   }
 }
